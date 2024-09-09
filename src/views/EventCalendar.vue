9ce9d4f634605bd7adc3595d1a65d941af59a624
creaper9487 <58578328+creaper9487@users.noreply.github.com>
2024-09-09 12:56:41 +0800

    fixed calendar and finished detail. New json column added

--- src/views/EventCalendar.vue
+++ src/views/EventCalendar.vue
@@ -13,45 +13,52 @@ const eventList = calStore.events
 
 <template>
   <div class="p-12">
-    <p class="text-[32px]">活動行事曆</p>
+    <p class="text-[32px] fixed">活動行事曆</p>
 
-    <div class="py-12">
+    <div class=" flex-wrap py-12">
       <ul v-if="$route.query.tag == null" v-for="item in eventList" :key="item.id">
-        <li class="flex py-3">
-          <img :src="item.img" alt="event image" class="w-10 h-10 object-cover">
+        <li class="flex-grow py-2">
+          <img :src="item.img" alt="event image" class="w-50 h-30 object-cover">
           <RouterLink :to="{
             name: 'event-detail',
             query: {
               evnt: item.name
             }
-          }"><p class="ml-1rem font-bold text-cyan-500">{{ item.title }}</p></RouterLink>
-          <p class="ml-1 mr-2">{{ item.date }}</p>
-          <div class=" w-1">
-            <RouterLink :to="{
+          }"><p class="ml-1rem flex-grow font-bold text-cyan-500">{{ item.title }}</p>
+          </RouterLink>
+          <p class="px-.5 text-secondary-950"> 活動日期： {{ item.date }}</p>
+          <ul class="flex py-2">
+            <li>
+              <RouterLink :to="{
                 name: 'event-calendar',
               query: {
                   tag: item.tag[0]
               }
-            }" class="bg-slate-400 rounded-md w-flex h-0.3 text-center ml-1 mr-0.5"
-              :style="{ width: `calc(${item.tag[0].length * 0.75}rem + 0.5rem)` }">{{ item.tag[0] }}
-            </RouterLink>
+              }" class="bg-slate-400 rounded-md h-fit w-fit text-center ml-1 mr-0.5 truncate"
+              >{{ item.tag[0] }}
+              </RouterLink>
+            </li>
+            <li>
             <RouterLink :to="{
               name: 'event-calendar',
               query: {
                 tag: item.tag[1]
               }
-            }" class="bg-slate-400 rounded-md w-flex h-0.3 text-center ml-1 mr-0.5"
-              :style="{ width: `calc(${item.tag[1].length * 0.75}rem + 0.5rem)` }">{{ item.tag[1] }}
+            }" class="bg-slate-400 rounded-md h-fit w-fit text-center ml-1 mr-0.5 truncate"
+              >{{ item.tag[1] }}
             </RouterLink>
+            </li>
+            <li>
             <RouterLink :to="{
               name: 'event-calendar',
               query: {
                 tag: item.tag[2]
               }
-            }" class="bg-slate-400 rounded-md w-flex h-0.3 text-center ml-1 mr-0.5"
-              :style="{ width: `calc(${item.tag[2].length * 0.75}rem + 0.5rem)` }">{{ item.tag[2] }}
+            }" class=" flex-grow bg-slate-400 rounded-md h-fit w-fit text-center ml-1 mr-0.5 truncate"
+              >{{ item.tag[2] }}
             </RouterLink>
-          </div>
+            </li>
+          </ul>
         </li>
       </ul>
       <ul v-if="$route.query.tag != ''" v-for="item in eventList" :key="item.id">
--- src/views/EventDetail.vue
+++ src/views/EventDetail.vue
@@ -10,40 +10,56 @@ const curEvent = eval('eventList[' + "route.query.evnt" + ']')  ;
 </script>
 
 <template>
-  <div class="bg-primary-500 block h-20 text-lg">
-    <a @click="$router.back()" class="inline-block align-middle"> go back go back</a>
-  </div>
   <div v-if="curEvent != null">
     <!-- put in event itself  -->
-    <div class="flex justify-center">
-      <div class="w-1/2">
-        <img :src="curEvent.img" alt="event image" class="w-full h-96 object-cover" />
-      </div>
-      <div class="w-1/2">
-        <p class="text-4xl">{{ curEvent.title }}</p>
-
-        <div class="flex">
-          <RouterLink
-            :to="{ name: 'event-calendar', query: { tag: curEvent.tag[0] } }"
-            class="bg-slate-400 rounded-md w-20 h-8 text-center ml-1 mr-0.5"
-            >{{ curEvent.tag[0] }}
-          </RouterLink>
-          <RouterLink
-            :to="{ name: 'event-calendar', query: { tag: curEvent.tag[1] } }"
-            class="bg-slate-400 rounded-md w-20 h-8 text-center ml-1 mr-0.5"
-            >{{ curEvent.tag[1] }}
-          </RouterLink>
-          <RouterLink
-            :to="{ name: 'event-calendar', query: { tag: curEvent.tag[2] } }"
-            class="bg-slate-400 rounded-md w-20 h-8 text-center ml-1 mr-0.5"
-            >{{ curEvent.tag[2] }}
-          </RouterLink>
-        </div>
+    <div class="flex-nowrap bg-slate-100 justify-center">
+        <img :src="curEvent.img" alt="event image" class="w-full h-fit object-cover" />
+      <div class="w-fit">
+        <p class="text-4xl">{{ curEvent.title }} ({{ curEvent.date }})</p>
+        <hr class="w-full border-2 border-primary-200" />
+        <a :href="'//' + curEvent.link" target="_blank" class="text-xl ">
+          活動詳細資訊
+        </a>
+        <hr class="w-full border-2 border-primary-200" />
+        <p class="text-xl">{{ curEvent.description }}</p>
+        <hr class="w-full border-2 border-primary-200" />
+        <ul class="flex py-2 text-lg">
+            <li>
+              <RouterLink :to="{
+                name: 'event-calendar',
+              query: {
+                  tag: curEvent.tag[0]
+              }
+              }" class="bg-slate-400 rounded-md h-fit w-fit text-center ml-1 mr-0.5 truncate"
+              >{{ curEvent.tag[0] }}
+              </RouterLink>
+            </li>
+            <li>
+            <RouterLink :to="{
+              name: 'event-calendar',
+              query: {
+                tag: curEvent.tag[1]
+              }
+            }" class="bg-slate-400 rounded-md h-fit w-fit text-center ml-1 mr-0.5 truncate"
+              >{{ curEvent.tag[1] }}
+            </RouterLink>
+            </li>
+            <li>
+            <RouterLink :to="{
+              name: 'event-calendar',
+              query: {
+                tag: curEvent.tag[2]
+              }
+            }" class=" flex-grow bg-slate-400 rounded-md h-fit w-fit text-center ml-1 mr-0.5 truncate"
+              >{{ curEvent.tag[2] }}
+            </RouterLink>
+            </li>
+          </ul>
       </div>
     </div>
   </div>
   <div v-else>
-    <!-- put in test itself  -->
+    <!-- test, should be cleaned b4 release  -->
     <div class="flex justify-center">
       <div class="w-1/2">
         <img :src="curEvent.img" alt="event image" class="w-full h-96 object-cover" />
